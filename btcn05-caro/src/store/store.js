79669157f9d2db99ef3ad2bd1bbab502addc052a
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2019-10-09 20:27:08 +0700

    fix store bug

--- btcn05-caro/src/store/store.js
+++ btcn05-caro/src/store/store.js
@@ -1,10 +1,5 @@
 import { createStore } from "redux";
 import Reducers from '../reducer/index';
 
-export default () => {
-    // Create store with reducers and initial state
-    const initialState = {};
-    const store = createStore(initialState, Reducers);
-
-    return store;
-};
+const store = createStore(Reducers);
+export default store
